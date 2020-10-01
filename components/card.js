2aa317aaec38425e10e9274d69b52fff948bc591
warisanwar <warismir07@gmail.com>
2020-10-01 20:38:12 +0530

    Added scroll to the cards

--- components/card.js
+++ components/card.js
@@ -7,48 +7,48 @@ export default function Card(props) {
   return (
     <div className={"card"}>
       <Link key={data.__resourcePath} href={formatPath(data.__resourcePath)}>
-          <div className={styles.card_inner}>
-            <div className="card-image">
-              <figure className="image">
-                <picture>
-                  <source
-                    srcSet={require(`../public/images/devs/${
-                      data.image || "user-placeholder.png"
+        <div className={styles.card_inner}>
+          <div className="card-image">
+            <figure className="image">
+              <picture>
+                <source
+                  srcSet={require(`../public/images/devs/${
+                    data.image || "user-placeholder.png"
                     }?webp`)}
-                    alt={data.name}
-                    type="image/webp"
-                  />
-                  <img className="image is-256x256"
-                    src={require(`../public/images/devs/${
-                      data.image || "user-placeholder.png"
+                  alt={data.name}
+                  type="image/webp"
+                />
+                <img className="image is-256x256"
+                  src={require(`../public/images/devs/${
+                    data.image || "user-placeholder.png"
                     }`)}
-                    alt={data.name}
-                  />
-                </picture>
-              </figure>
+                  alt={data.name}
+                />
+              </picture>
+            </figure>
+          </div>
+          <div className="card-content scroll">
+            <div className="media">
+              <div className="media-content">
+                <p className="title is-4">{data.name}</p>
+                <p className="subtitle is-6">{data.address}</p>
+              </div>
             </div>
-            <div className="card-content">
-              <div className="media">
-                <div className="media-content">
-                  <p className="title is-4">{data.name}</p>
-                  <p className="subtitle is-6">{data.address}</p>
-                </div>
+            <div className="content">
+              <div className={styles.tags}>
+                {data.tags.map((tag, idx) => (
+                  <span
+                    key={idx.toString()}
+                    className={`tag is-rounded ${styles.tag}`}
+                  >
+                    {tag}
+                  </span>
+                ))}
               </div>
-              <div className="content">
-                <div className={styles.tags}>
-                  {data.tags.map((tag, idx) => (
-                    <span
-                      key={idx.toString()}
-                      className={`tag is-rounded ${styles.tag}`}
-                    >
-                      {tag}
-                    </span>
-                  ))}
-                </div>
-                {data.description}
-              </div>
+              {data.description}
             </div>
           </div>
+        </div>
       </Link>
       <footer className="card-footer">
         <div className={styles.links}>
